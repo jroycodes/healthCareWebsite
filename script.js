f172ddc0564463a0c45a1b9a0f178c1ef2b4e60b
Jonathan Roy <jonaisroy@gmail.com>
2023-10-16 18:57:46 +0300

    Added function to close sidebar

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function openNav() {
   document.getElementById("main").style.marginRight = "250px";
   document.getElementById("button").classList.add("hide");
 }
+
+// Closes sidebar when clicked
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.getElementById("button").classList.remove("hide");
+}
