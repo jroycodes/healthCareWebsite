ad939a7c7bd6deab249cb6403a55e633777572e8
Jonathan Roy <jonaisroy@gmail.com>
2023-10-16 18:55:15 +0300

    Added open nav function

---
+++ script.js
@@ -0,0 +1,6 @@
+// Opens sidebar when clicked
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("button").classList.add("hide");
+}
